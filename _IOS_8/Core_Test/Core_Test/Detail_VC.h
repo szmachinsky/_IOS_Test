d3ef540316e15dc35b5f1aef1cd9ecf63458c4bb
Zmachinsky <sergei.zma.bp@gmail.com>
2015-03-06 10:36:55 +0300

    core migration example

---
+++ _IOS_8/Core_Test/Core_Test/Detail_VC.h
@@ -0,0 +1,16 @@
+//
+//  Detail_VC.h
+//  Core_Test
+//
+//  Created by Zmachinsky Sergei on 05.03.15.
+//  Copyright (c) 2015 Zmachinsky Sergei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detail_VC : UIViewController
+
+@property (weak, nonatomic) id delegate;
+@property (strong, nonatomic) id detailItem;
+
+@end
