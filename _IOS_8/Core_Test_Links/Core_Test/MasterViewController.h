8ae2c9144b7404ae613a7bdb79a3fe82b900f7e6
Zmachinsky <sergei.zma.bp@gmail.com>
2015-03-16 13:55:32 +0300

    work

---
+++ _IOS_8/Core_Test_Links/Core_Test/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Core_Test
+//
+//  Created by Zmachinsky Sergei on 05.03.15.
+//  Copyright (c) 2015 Zmachinsky Sergei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
