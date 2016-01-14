6d422926157e6c440f050fbfe776a2cec732af7d
Zmachinsky Sergei <sergei.zmachinsky@belprog.com>
2016-01-14 10:36:31 +0300

    work

--- _IOS_8/test_nav_xib/test_nav_xib/AppDelegate.m
+++ _IOS_8/test_nav_xib/test_nav_xib/AppDelegate.m
@@ -322,20 +322,22 @@ uint64_t getTickCount(void)
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    NSLog(@"+++++++++++++++0-- Will_Resign_Active --");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    NSLog(@"+++++++++++++++1-- Will_Enter_Background --");
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    NSLog(@"---1----");
+    NSLog(@"+++++++++++++++2-- Will_Enter_Foreground --");
 //    NSNotification *anote1 = [NSNotification notificationWithName:@"kRefreshAfterBackgroundState" object:nil userInfo:nil];
 //    [[NSNotificationCenter defaultCenter] postNotification:anote1];
     
@@ -350,7 +352,7 @@ uint64_t getTickCount(void)
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    NSLog(@"---2----");
+    NSLog(@"+++++++++++++++3-- Did___Become_Active --");
     
 //    NSNotification *anote1 = [NSNotification notificationWithName:@"kRefreshAfterBackgroundState" object:nil userInfo:nil];
 //    [[NSNotificationCenter defaultCenter] postNotification:anote1];
